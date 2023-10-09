247c450f749f15712ec8c4daea97210d086ffe36
mael.tourres@gmail.com <mael.tourres@gmail.com>
2023-10-09 18:48:00 +0200

    added py parser

--- barebones/src/helloworld.c
+++ barebones/src/helloworld.c
@@ -28,13 +28,5 @@ int main()
 {
     init_uart(50000000, 115200);
     print_uart("Hello World FROM RISCV BAREBONE\r\n");
-    
-    print_uart("After initialisation\r\n");
-    int test=0 ; 
-    test= template(1,2); 
-    print_uart_byte(test);
-    print_uart("After addition\r\n");
-    
-
     return 0;
 }
